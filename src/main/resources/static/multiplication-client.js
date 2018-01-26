3709e6fa5963592cae0cab62db61c2af5e0d2f7f
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-01-26 18:23:52 -0200

    show stats from last attempts

--- src/main/resources/static/multiplication-client.js
+++ src/main/resources/static/multiplication-client.js
@@ -16,6 +16,22 @@ function updateMultiplication(){
 	});
 }
 
+function updateStats(alias){
+	$.ajax({
+		url: "http://localhost:8080/results/" + alias,
+	}).then(function(data){
+		$('#stats-body').empty();
+		data.forEach(function(row){
+			$('#stats-body').append('<tr>' + 
+					'<td>' + row.id + '</td>' +
+					'<td>' + row.multiplication.factorA + ' x ' + row.multiplication.factorB + '</td>' +
+					'<td>' + row.resultAttempt + '</td>' +
+					'<td>' + (row.correct === true ? 'YES' : 'NO') + '</td>' + 
+			'</tr>');
+		});
+	});
+}
+
 $(document).ready(function(){
 	updateMultiplication();
 	
@@ -54,5 +70,6 @@ $(document).ready(function(){
 		});
 		
 		updateMultiplication();
+		updateStats(userAlias);
 	});
 });
